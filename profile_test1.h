13c51b9412b46e9d13b546134db1b451654edc4e
guyco75 <x>
2018-07-07 20:22:12 +0300

    refactor profile_test1.h

--- profile_test1.h
+++ profile_test1.h
@@ -3,67 +3,57 @@
 #define RS_ARRAY_SIZE (2)
 #include "roller_shutter.h"
 
-unsigned long xx[] = {0x0};
-unsigned long uu[] = {0x1};
-unsigned long dd[] = {0x2};
+unsigned long single_click[] = {(100),0};
+unsigned long double_click[] = {(100),100,(100),0};
+unsigned long triple_click[] = {(100),100,(100),100,(100),0};
+unsigned long quadruple_click[] = {(100),100,(100),100,(100),100,(100),0};
+unsigned long pentuple_click[] = {(100),100,(100),100,(100),100,(100),100,(100),0};
 
-unsigned long single_click[] = {4000,(100),0};
-unsigned long double_click[] = {100,(100),100,(100),0};
-unsigned long triple_click[] = {100,(100),100,(100),100,(100),0};
-unsigned long quadruple_click[] = {100,(100),100,(100),100,(100),100,(100),0};
-unsigned long pentuple_click[] = {100,(100),100,(100),100,(100),100,(100),100,(100),0};
+unsigned long triple_long_click[] = {(100),100,(100),100,(100),100,(1000),0};
+unsigned long quadruple_long_click[] = {(100),100,(100),100,(100),100,(100),100,(1000),0};
+unsigned long pentuple_long_click[] = {(100),100,(100),100,(100),100,(100),100,(100),100,(1000),0};
 
-unsigned long triple_long_click[] = {100,(100),100,(100),100,(100),100,(1000),0};
-unsigned long quadruple_long_click[] = {100,(100),100,(100),100,(100),100,(100),100,(1000),0};
-unsigned long pentuple_long_click[] = {100,(100),100,(100),100,(100),100,(100),100,(100),100,(1000),0};
+unsigned long long_click[] = {(7000),0};
 
-unsigned long long_click[] = {4000,(7000),0};
-
-static unsigned long last_action;
-unsigned long *test_vector[][2] = {
-/*  {uu, single_click},
-  {uu, double_click},
-  {uu, triple_click},
-  {uu, quadruple_click},
-  {uu, pentuple_click},
-
-  {uu, triple_long_click},
-  {uu, quadruple_long_click},
-  {uu, pentuple_long_click},*/
-  {uu, long_click},
-  {uu, double_click},
-  {dd, long_click},
-  {uu, double_click},
-  {uu, single_click},
-  {uu, double_click},
-  {xx, NULL},
+struct test_step {
+  uint8_t button_mask;
+  unsigned long *click_sequence;
+  unsigned long w;
 };
 
-uint8_t p;
+static unsigned long long int last_action;
+struct test_step test_vector[] = {
+  {0x1, long_click,   4000},
+  {0x1, double_click, 7000},
+  {0x2, long_click,   1000},
+  {0x1, double_click, 4000},
+  {0x1, single_click, 1000},
+  {0x1, double_click, 8000},
+  {0x0, NULL,         0},
+};
 
 void loop();
 void unit_test1() {
   int test = 0;
   for (;;) {
-    if (test_vector[test][0][0]) {
-      last_action = millis();
-      int i = 0;
-      while (test_vector[test][1][i]) {
-        if (millis() - last_action >= test_vector[test][1][i]) {
-          p ^= test_vector[test][0][0];
-          last_action = millis();
-          ++i;
-        }
-        loop();
-      }
-      for (int t=0; t<100; ++t) {
-        loop();
-      }
-      Serial.println("---------");
-      ++test;
-    } else {
+    if (!test_vector[test].click_sequence)
       break;
+
+    analog_pins ^= test_vector[test].button_mask;
+    last_action = millis();
+    int i = 0;
+    while (test_vector[test].click_sequence[i]) {
+      if (millis() - last_action >= test_vector[test].click_sequence[i]) {
+        analog_pins ^= test_vector[test].button_mask;
+        last_action = millis();
+        ++i;
+      }
+      loop();
     }
+    while (millis() - last_action < test_vector[test].w)
+      loop();
+    Serial.println("---------");
+    ++test;
   }
 }
 
@@ -83,11 +73,9 @@ void unit_test2() {
 }
 
 void setup_rs() {
-  p = 0xF;
-
-  //pin port, btn_up, btn_dn, relay_up, relay_dn
-  rs[0].setup(0, &p, 0, 1, 7, 8);
-  rs[1].setup(1, &p, 2, 3, 9, 10);
+  //id, btn_up, btn_dn, relay_up, relay_dn
+  rs[0].setup(0, A0, A1, 7, 8);
+  rs[1].setup(1, A2, A3, 9, 10);
 
   unit_test1();
   unit_test2();
